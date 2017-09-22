d70a3cd4ca4b7916944f27636906d4f35509a6e3
theonemlst <johnyjohn018@gmail.com>
2017-09-22 12:47:51 +0300

    Upload project to repository

---
+++ app/src/main/java/com/example/a1/currencyrates/GetJson.java
@@ -0,0 +1,46 @@
+package com.example.a1.currencyrates;
+
+import org.json.*;
+import java.io.*;
+import java.net.*;
+
+class GetJson {
+
+	public String getRate() {
+		
+		URL url;
+		InputStream is = null;
+		BufferedReader br = null;
+		String line;
+		String price = "";
+		String result = "";
+		PrintWriter os = null;
+
+		try {
+			url = new URL("https://api.cryptonator.com/api/ticker/eth-usd");
+			is = url.openStream();  // throws an IOException
+			br = new BufferedReader(new InputStreamReader(is));
+
+			while ((line = br.readLine()) != null) {								
+				result += line;
+			}
+		} catch (MalformedURLException mue) {
+			 mue.printStackTrace();
+		} catch (IOException ioe) {
+			 ioe.printStackTrace();
+		} finally {
+			try {
+				if (br != null) br.close();
+			} catch (IOException ioe) {
+				// nothing to see here
+			}
+		}
+		try {
+			JSONObject obj = new JSONObject(result);
+			price = obj.getJSONObject("ticker").getString("price");
+		} catch (JSONException exc) {}
+
+		return price;
+	}
+
+}
---
+++ app/src/main/java/com/example/a1/currencyrates/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.a1.currencyrates;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView helloTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helloTV = (TextView) findViewById(R.id.hello_tv);
+        Button helloBTN = (Button) findViewById(R.id.hello_btn);
+
+        helloBTN.setOnClickListener(onClickListener);
+        helloTV.setOnClickListener(onClickListener);
+
+        new LongOperation().execute("");
+    }
+
+    private class LongOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            GetJson getJson = new GetJson();
+            String rate = getJson.getRate();
+            //helloTV.setText(rate);
+            return rate;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView txt = (TextView) findViewById(R.id.hello_tv);
+            txt.setText(String.format(Locale.ENGLISH, "%(.2f", Double.parseDouble(result))); // txt.setText(result);
+            // might want to change "executed" for the returned string passed
+            // into onPostExecute() but that is upto you
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.hello_btn:
+                    new LongOperation().execute("");
+                    break;
+            }
+        }
+    };
+}
